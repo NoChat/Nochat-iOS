807c438cc9b564f3e9e894600e23093efc4afb19
Rangken <rangken@gmail.com>
2015-02-24 02:24:06 +0900

    Initial Commit

---
+++ nochat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nochat
+//
+//  Created by Rangken on 2015. 2. 24..
+//  Copyright (c) 2015년 Nexters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ nochat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  nochat
+//
+//  Created by Rangken on 2015. 2. 24..
+//  Copyright (c) 2015년 Nexters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
